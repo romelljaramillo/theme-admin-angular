29f34ca4fdbc33eb29efddc7de8d898c54dcda5e
EXIS - Romell Antonio Jaramillo Araque <romeLl.jaramillo@altia.santiago>
2022-05-31 13:28:49 +0200

    theme base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './admin/template/header/header.component';
+import { FooterComponent } from './admin/template/footer/footer.component';
+import { NavComponent } from './admin/template/nav/nav.component';
+import { ContentComponent } from './admin/template/content/content.component';
+import { LayoutComponent } from './admin/template/layout/layout.component';
+import { LeftMenuComponent } from './admin/template/left-menu/left-menu.component';
+import { BreadcrumbComponent } from './admin/template/breadcrumb/breadcrumb.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    ContentComponent,
+    LayoutComponent,
+    LeftMenuComponent,
+    BreadcrumbComponent
   ],
   imports: [
     BrowserModule,
